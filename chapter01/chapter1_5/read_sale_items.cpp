555a75ca4b48092613b95709f677abb98e57a44c
zhangfei <zhangfei@hoodinn.com>
2022-06-22 11:37:35 +0800

    chapter1.5

---
+++ chapter01/chapter1_5/read_sale_items.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "sale_items.h"
+
+// input : 0-201-70353-X 4 24.99
+
+int main()
+{
+    Sales_item book;
+    std::cin >> book;
+    std::cout << book << std::endl;
+    return 0;
+}
---
+++ chapter01/chapter1_5/sum_sale_items.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "sale_items.h"
+
+// input :
+// 0-201-70353-X 4 24.99
+// 0-201-78345-X 2 25.00
+
+int main()
+{
+    Sales_item item1, item2;
+    std::cin >> item1 >> item2;              // 读取交易记录
+    std::cout << item1 + item2 << std::endl; // 打印和
+    return 0;
+}
